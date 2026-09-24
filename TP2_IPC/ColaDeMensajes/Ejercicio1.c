126497b63075eb6b1f72430535de00fe8b1c5683
Seba <siag.inge4@gmail.com>
2026-09-24 12:15:40 -0300

    EJ1 y EJ2 cleancode

--- TP2_IPC/ColaDeMensajes/Ejercicio1.c
+++ TP2_IPC/ColaDeMensajes/Ejercicio1.c
@@ -1,145 +1,67 @@
-#include <stdio.h> // printf, fprintf
-#include <stdlib.h> // malloc, exit, random
-#include <unistd.h> // fork, exec, getpid, sleep
-#include <sys/wait.h> // wait, waitpid
-#include <pthread.h> // hilos POSIX
-#include <sys/types.h> // pid_t y otros tipos
-#include <time.h> // time, medicion de tiempo
-#include <stdint.h> // enteros de tamaño fijo
-#include <sys/mman.h> // memoria compartida (mmap)
-#include <fcntl.h> // flags de apertura (O_CREAT, etc.)
-#include <sys/stat.h> // permisos de archivos
-#include <sys/shm.h> // memoria compartida System V
-#include <string.h> // manejo de strings
-#include <sys/time.h> // gettimeofday
-#include <math.h> // funciones matematicas
-#include <dirent.h> // lectura de directorios
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
 #include <sys/msg.h>
 
-#define KEY ((key_t)(143))
-#define CANT 10
-
-/*
-    Cola de mensajes con tipos: el proceso principal crea la cola y
-    envia varios mensajes de dos tipos distintos. Cada uno de los
-    otros dos procesos lee iterativamente uno de los tipos de mensaje
-    y lo muestra por pantalla, esperando si no hay mensajes disponibles.
-*/
-
-/*
-    Una caracteristica de las colas de mensajes
-    es que los mensajes pueden clasificarse
-    utilizando tipos.
-    Crear tres procesos tal que:
-
-    - El procesos principal debe enviar varios mensajes
-    de dos tipos diferentes utilizando una
-    cola de mensajes creada previamente.
-
-    - Cada uno de los restantes procesos lee iterativamente
-    uno de los tipos de mensaje y lo muestra por pantalla.
-
-    - Si no existen mensajes en la cola se debe esperar.
-
-*/
+#define KEY ((key_t)(1243))
 
 struct mensaje {
     long tipo;
     int dato;
 };
 
-int main()
-{
-
-    pid_t pid1;
-    pid_t pid2;
-
+int main() {
     srandom(time(NULL));
 
     int msqid = msgget(KEY, IPC_CREAT | 0666);
-
-    int tipo1 = 0;
-    int tipo2 = 0;
-
     if (msqid == -1) {
-        printf("La cola no pudo crearse");
+        printf("La cola no pudo crearse\n");
         exit(1);
     }
 
-    printf("Soy el proceso padre mi pid es: %d \n", getpid());
+    pid_t pid;
 
-    //llenar la cola de mensajes, con dos mensajes diferentes de tipos diferentes.
-    for (int i = 0; i < CANT; i++) {
+    for (int i = 1; i < 3; i++) {
+        pid = fork();
 
-        struct mensaje mensajes[CANT];
-        int tip = random() % 2 + 1;
-        mensajes[i].tipo = tip;
+        if (pid == 0) {
+            int cantidadMensajes = 0;
+            while (1) {
+                struct mensaje msg;
+                int longitud = sizeof(struct mensaje) - sizeof(long);
 
-        if (tip == 1) {
-            tipo1++;
-        } else {
-            tipo2++;
-        }
+                if (msgrcv(msqid, &msg, longitud, i, 0) == -1) {
+                    perror("msgrcv");
+                    exit(1);
+                }
 
-        // strcpy(mensajes[i].texto, "Hola hijo %d, soy de tipo %d", mensajes[i].tipo, mensajes[i].tipo);
-        mensajes[i].dato = random() % 5;
-        int longitud = sizeof(struct mensaje) - sizeof(long);
-        msgsnd(msqid, &mensajes[i], longitud, 0);
-    }
-
-    pid1 = fork();
-
-    if (pid1 < 0) {
-        printf("Error");
-        exit(1);
-    }
-
-    if (pid1 == 0) {
-        //---- Proceso hijo 1 ----
-
-        printf("Soy el hijo 1 mi pid es: %d mi padre es %d \n", getpid(), getppid());
-
-        struct mensaje mensajes1[tipo1];
-        int longitud1;
-
-        for (int i = 0; i < tipo1; i++) {
-            longitud1 = sizeof(struct mensaje) - sizeof(long);
-            msgrcv(msqid, &mensajes1[i], longitud1, 1, 0);
-            printf("Recibi el mensaje: %d, soy de tipo %ld \n", mensajes1[i].dato, mensajes1[i].tipo);
-        }
-
-        exit(0);
-
-    } else if (pid1 > 0) {
-
-        pid2 = fork();
-
-        if (pid2 < 0) {
-            printf("Error");
-            exit(1);
-        }
-
-        if (pid2 == 0) {
-            //---- Proceso hijo 2----
-
-            printf("Soy el hijo 2 mi pid es: %d mi padre es %d \n", getpid(), getppid());
-
-            struct mensaje mensajes2[tipo2];
-            int longitud2;
-
-            for (int i = 0; i < tipo2; i++) {
-                longitud2 = sizeof(struct mensaje) - sizeof(long);
-                msgrcv(msqid, &mensajes2[i], longitud2, 2, 0);
-                printf("Recibi el mensaje: %d, soy de tipo %ld \n", mensajes2[i].dato, mensajes2[i].tipo);
+                printf("[%d] Soy el proceso %d Recibi el mensaje %d soy de tipo %ld \n", cantidadMensajes, i, msg.dato, msg.tipo);
+                cantidadMensajes++;
             }
-            
-            exit(0);
         }
-
-        //espera por ambos hijos
-        wait(NULL);
-        wait(NULL);
     }
 
+    int cantidadMensajes = 0;
+
+    while (1) {
+        struct mensaje msg;
+        int longitud = sizeof(struct mensaje) - sizeof(long);
+
+        msg.tipo = random() % 2 + 1;
+        msg.dato = random() % 9;
+
+        if (msgsnd(msqid, &msg, longitud, 0) == -1) {
+            printf("Error al mandar el mensaje\n");
+        }
+
+        printf("[%d] Se envio mensajes a la cola de tipo %ld el mensaje es %d \n", cantidadMensajes, msg.tipo, msg.dato);
+        cantidadMensajes++;
+
+        sleep(1);
+    }
+
+    msgctl(msqid, IPC_RMID, NULL);
     return 0;
 }
--- TP2_IPC/ColaDeMensajes/Ejercicio2.c
+++ TP2_IPC/ColaDeMensajes/Ejercicio2.c
@@ -17,21 +17,12 @@
 #include <sys/msg.h>
 
 #define KEY ((key_t)(1243))
-#define CANT 30
 
 /*
     Extension del ejercicio anterior de colas de mensajes: se agrega
     un cuarto proceso que puede leer cualquiera de los dos tipos de
     mensaje. Al finalizar, cada proceso indica la cantidad de mensajes
     que leyo.
-*/
-
-/*
-    - IPC_NOWAIT: si no hay ningun mensaje del tipo pedido en la cola,
-
-    - msgrcv() no bloquea al proceso -> devuelve -1 (errno = ENOMSG) al instante.
-
-    - Sin este flag, msgrcv() se queda esperando (bloqueado) hasta que llegue un mensaje de ese tipo.
 */
 
 struct mensaje {
@@ -42,125 +33,65 @@ struct mensaje {
 int main()
 {
 
-    pid_t pid1;
-    pid_t pid2;
-    pid_t pid3;
-
     srandom(time(NULL));
 
+    //Creo la cola de mensajes
     int msqid = msgget(KEY, IPC_CREAT | 0666);
-
-    int tipo1 = 0;
-    int tipo2 = 0;
-
     if (msqid == -1) {
-        printf("La cola no pudo crearse");
-        exit(1);
-    }
-    
-    printf("Soy el proceso padre mi pid es: %d \n", getpid());
-
-    // llenar la cola de mensajes, con dos mensajes diferentes de tipos diferentes.
-    for (int i = 0; i < CANT; i++) {
-        struct mensaje mensajes[CANT];
-        int tip = random() % 2 + 1;
-        mensajes[i].tipo = tip;
-        // strcpy(mensajes[i].texto, "Hola hijo %d, soy de tipo %d", mensajes[i].tipo, mensajes[i].tipo);
-        mensajes[i].dato = random() % 5;
-        int longitud = sizeof(struct mensaje) - sizeof(long);
-        msgsnd(msqid, &mensajes[i], longitud, 0);
-    }
-
-    pid3 = fork();
-
-    if (pid3 < 0) {
-        printf("Error");
+        printf("La cola no pudo crearse\n");
         exit(1);
     }
 
-    if (pid3 == 0) {
-        int leidos = 0;
-        printf("Soy el hijo 3 mi pid es: %d mi padre es %d \n", getpid(), getppid());
+    pid_t pid;
 
-        struct mensaje mensajes3[CANT];
-        int longitud3;
-        for (int i = 0; i < CANT; i++) {
-            longitud3 = sizeof(struct mensaje) - sizeof(long);
-            if (msgrcv(msqid, &mensajes3[i], longitud3, 0, IPC_NOWAIT) == -1) {
-                break;
-            } else {
-                leidos++;
+    //Creo 3 hijos, que van a recibir de tipos:
+    //tipo 0, recibi cualquier tipo
+    //tipo 1, de tipo 1
+    //tipo 2, recibe de tipo 2
+    //lo bueno que al usar el i del for, ya contemplo todo
+    for (int i=0; i < 3; i++){
+
+        pid = fork ();  //apenas se ejecuta el FORK, deja de existir, el for y pasa a la instruccion del hijo
+
+        if (pid == 0) {
+            //soy uno de los hijos
+            int cantidadMensajes = 0;
+            while(1){
+            
+                struct mensaje msg;
+                int longitud = sizeof(struct mensaje) - sizeof(long);
+
+                if (msgrcv(msqid, &msg, longitud, i, 0) == -1) { 
+                    perror("msgrcv"); exit(1); }
+                
+                printf("[%d] Soy el proceso %d Recibi el mensaje %d soy de tipo %ld \n",cantidadMensajes, i, msg.dato, msg.tipo );
+                cantidadMensajes++;
             }
-            printf("Proceso 3: Recibi el mensaje: %d, soy de tipo %ld \n", mensajes3[i].dato, mensajes3[i].tipo);
-        }
-        printf("Proceso 3: Lei %d procesos\n", leidos);
-        exit(0);
-
-    } else {
-
-        pid1 = fork();
-
-        if (pid1 < 0) {
-            printf("Error");
-            exit(1);
-        }
-
-        if (pid1 == 0) {
-            //---- Proceso hijo 1 ----
-            int leidos = 0;
-            printf("Soy el hijo 1 mi pid es: %d mi padre es %d \n", getpid(), getppid());
-
-            struct mensaje mensajes1[CANT];
-            int longitud1;
-            for (int i = 0; i < CANT; i++) {
-                longitud1 = sizeof(struct mensaje) - sizeof(long);
-                if (msgrcv(msqid, &mensajes1[i], longitud1, 1, IPC_NOWAIT) == -1) {
-                    break;
-                } else {
-                    leidos++;
-                }
-                printf("Proceso1: Recibi el mensaje: %d, soy de tipo %ld \n", mensajes1[i].dato, mensajes1[i].tipo);
-            }
-            printf("Proceso1: Lei %d procesos \n", leidos);
-            exit(0);
-
-        } else if (pid1 > 0) {
-
-            pid2 = fork();
-
-            if (pid2 < 0) {
-                printf("Error");
-                exit(1);
-            }
-
-            if (pid2 == 0) {
-                //---- Proceso hijo 2----
-                int leidos = 0;
-                printf("Soy el hijo 2 mi pid es: %d mi padre es %d \n", getpid(), getppid());
-
-                struct mensaje mensajes2[CANT];
-                int longitud2;
-                for (int i = 0; i < CANT; i++) {
-                    longitud2 = sizeof(struct mensaje) - sizeof(long);
-
-                    if (msgrcv(msqid, &mensajes2[i], longitud2, 2, IPC_NOWAIT) == -1) {
-                        break;
-                    } else {
-                        leidos++;
-                    }
-
-                    printf("Proceso2: Recibi el mensaje: %d, soy de tipo %ld \n", mensajes2[i].dato, mensajes2[i].tipo);
-                }
-                printf("Proceso2: Lei %d procesos\n", leidos);
-                exit(0);
-            }
-
-            // espera por ambos hijos
-            wait(NULL);
-            wait(NULL);
-            wait(NULL);
         }
     }
 
+    int cantidadMensajes = 0;
+
+    while (1) {
+        int i=0;
+        struct mensaje msg;
+        int longitud = sizeof(struct mensaje) - sizeof(long); // NO TOCAR: el tipo no cuenta como dato
+
+        msg.tipo = random() % 2 + 1;
+        msg.dato = random() % 9;
+
+
+        //Una vez armado el mensaje, lo mando, chequeando por las dudas si da error.
+        if (msgsnd(msqid, &msg, longitud, 0) == -1) {
+            printf("Error al mandar el mensaje\n");
+        }
+
+        printf ("[%d] Se envio mensajes a la cola de tipo %ld el mensaje es %d \n" , cantidadMensajes, msg.tipo, msg.dato );
+        cantidadMensajes++;
+
+        sleep(1); 
+    }
+
+    msgctl(msqid, IPC_RMID, NULL);
     return 0;
 }
